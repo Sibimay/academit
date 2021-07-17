212ab7248fe08829184604e8003d32ab5f2f7f31
Василий Игнатков <sibimaqy@gmail.com>
2021-07-17 14:36:45 +0700

    Vector. Add methods

--- Vector/src/ru/academits/ignatkov/vector/Vector.java
+++ Vector/src/ru/academits/ignatkov/vector/Vector.java
@@ -3,42 +3,164 @@ package ru.academits.ignatkov.vector;
 import java.util.Arrays;
 
 public class Vector {
-    private int vectorDimension;
-    private final double[] vectorComponents;
+    private final int dimension;
+    private double[] components;
 
     public Vector(int n) {
         if (n <= 0) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("Размерность вектора должна быть больше 0");
         }
 
-        vectorDimension = n;
-        vectorComponents = new double[vectorDimension];
-        Arrays.fill(vectorComponents, 0);
+        dimension = n;
+        components = new double[dimension];
+        Arrays.fill(components, 0);
     }
 
     public Vector(Vector vector) {
-        this(vector.vectorDimension, vector.vectorComponents);
+        this(vector.dimension, vector.components);
     }
 
     public Vector(double[] components) {
-        vectorComponents = components;
+        if (components.length <= 0) {
+            throw new IllegalArgumentException("Размерность вектора должна быть больше 0");
+        }
+
+        this.components = components;
+        dimension = this.components.length;
     }
 
     public Vector(int n, double[] components) {
-        vectorDimension = n;
-        vectorComponents = new double[vectorDimension];
+        if (n <= 0 || components.length <= 0) {
+            throw new IllegalArgumentException("Размерность вектора должна быть больше 0");
+        }
+
+        dimension = n;
+        this.components = new double[dimension];
 
         for (int i = 0; i < n; i++) {
-            vectorComponents[i] = components.length - 1 >= i ? components[i] : 0;
+            this.components[i] = components.length - 1 >= i ? components[i] : 0;
         }
     }
 
     public int getSize() {
-        return vectorDimension;
+        return dimension;
+    }
+
+    public double[] getComponents() {
+        return components;
     }
 
     @Override
     public String toString() {
-        return Arrays.toString(vectorComponents).replace("[", "{").replace("]", "}");
+        return Arrays.toString(components).replace("[", "{").replace("]", "}");
+    }
+
+    public void addVector(Vector vector) {
+        Vector maxDimensionVector = dimension > vector.dimension ? this : vector;
+        Vector minDimensionVector = dimension > vector.dimension ? vector : this;
+
+        if (this == minDimensionVector) {
+            components = Arrays.copyOf(components, vector.dimension);
+        }
+
+        for (int i = 0; i < maxDimensionVector.dimension; i++) {
+            components[i] += vector.components[i];
+        }
+    }
+
+    public void subtractVector(Vector vector) {
+        Vector maxDimensionVector = dimension > vector.dimension ? this : vector;
+        Vector minDimensionVector = dimension > vector.dimension ? vector : this;
+
+        if (this == minDimensionVector) {
+            components = Arrays.copyOf(components, vector.dimension);
+        }
+
+        for (int i = 0; i < maxDimensionVector.dimension; i++) {
+            components[i] -= vector.components[i];
+        }
+    }
+
+    public void multipleOnScalar(double number) {
+        for (int i = 0; i < dimension; i++) {
+            components[i] *= number;
+        }
+    }
+
+    public void reverse() {
+        multipleOnScalar(-1);
+    }
+
+    public double getComponent(int index) {
+        if (index < 0 || index > dimension) {
+            throw new IllegalArgumentException("Индекс не может быть отрицательным " +
+                    "и не может быть больше, чем размерность вектора");
+        }
+
+        return components[index];
+    }
+
+    public void setComponent(int index, double value) {
+        if (index < 0 || index > dimension) {
+            throw new IllegalArgumentException("Индекс не может быть отрицательным " +
+                    "и не может быть больше, чем размерность вектора");
+        }
+
+        components[index] = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Vector vector = (Vector) o;
+
+        return dimension == vector.dimension && Arrays.equals(components, vector.components);
+    }
+
+    @Override
+    public int hashCode() {
+        double componentsSum = 0;
+
+        for (double component : components) {
+            componentsSum += component;
+        }
+
+        return (int) (dimension * componentsSum);
+    }
+
+    public static Vector getVectorsSum(Vector vector1, Vector vector2) {
+        vector1.addVector(vector2);
+
+        return new Vector(vector1);
+    }
+
+    public static Vector getVectorsDifference(Vector vector1, Vector vector2) {
+        vector1.subtractVector(vector2);
+
+        return new Vector(vector1);
+    }
+
+    public static Vector getVectorsScalarMultiplication(Vector vector1, Vector vector2) {
+        Vector maxDimensionVector = vector1.dimension > vector2.dimension ? vector1 : vector2;
+        Vector minDimensionVector = vector1.dimension > vector2.dimension ? vector2 : vector1;
+        Vector resultVector = new Vector(maxDimensionVector);
+
+        for (int i = 0; i < maxDimensionVector.dimension; i++) {
+            if (minDimensionVector.dimension <= i) {
+                resultVector.components[i] = 0;
+                continue;
+            }
+
+            resultVector.components[i] = vector1.components[i] * vector2.components[i];
+        }
+
+        return resultVector;
     }
 }
--- Vector/src/ru/academits/ignatkov/vector_main/Main.java
+++ Vector/src/ru/academits/ignatkov/vector_main/Main.java
@@ -4,8 +4,17 @@ import ru.academits.ignatkov.vector.Vector;
 
 public class Main {
     public static void main(String[] args) {
-        double[] a = new double[] {1,2};
-        Vector vector = new Vector(3, a);
-        System.out.println(vector);
+        double[] a = {1, 2};
+        Vector vector1 = new Vector(3, a);
+        Vector vector2 = new Vector(4, new double[]{3, 7, 3, 4});
+
+        vector1.addVector(vector2);
+        System.out.println("После сложения векторов получился вектор " + vector1);
+
+        // Вернем первый вектор к изначальному состоянию
+        vector1 = new Vector(3, a);
+
+        vector1.subtractVector(vector2);
+        System.out.println("После вычитания векторов получится вектор " + vector1);
     }
 }
